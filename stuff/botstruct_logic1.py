228569df0d1fd4c409e0df89eaf69715bb9f8762
hoodielive <hoodielive@gmail.com>
2023-09-15 09:50:33 -0400

    ReOrg

--- stuff/botstruct_logic1.py
+++ stuff/botstruct_logic1.py
@@ -1,16 +1,82 @@
 import alpaca_trade_api as tradeapi
+import pandas as pd
 
 API_KEY = 'YOUR_ALPACA_API_KEY'
 SECRET_KEY = 'YOUR_ALPACA_SECRET_KEY'
 BASE_URL = 'https://paper-api.alpaca.markets'  # Use paper trading endpoint for testing
 
 api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
-
-# ... rest of your code ...
+df = pd.DataFrame()
 
 class TradingStrategy:
-    
-    # ... rest of the class ...
+
+    def __init__(self, asset):
+        self.asset = asset
+
+    def check_position(self):
+        # Logic to check if a position exists for the asset
+        pass
+
+    def is_tradable(self):
+        # Logic to check if the asset is tradable
+        pass
+
+    def load_data(self, timeframe):
+        # Load data for the given timeframe
+        pass
+
+    def get_trend(self, data):
+        # Analyze data to determine the trend
+        pass
+
+    def get_rsi(self, data):
+        # Calculate RSI based on data
+        pass
+
+    def get_stochastic(self, data):
+        # Calculate stochastic oscillator value based on data
+        pass
+
+    def enter_trade(self, decision):
+        # Logic to enter the trade based on decision (buy/sell/hold)
+        pass
+
+    def submit_order(self):
+        # Logic to submit the trade order
+        pass
+
+    def position_mode(self):
+        # Logic for position mode, i.e., monitoring the trade
+        pass
+
+    def decide_trade(self):
+        # Check position
+        if not self.check_position() and self.is_tradable():
+            data_30min = self.load_data('30min')
+            general_trend = self.get_trend(data_30min)
+
+            if general_trend == "clear_trend":
+                data_5min = self.load_data('5min')
+                instant_trend = self.get_trend(data_5min)
+                rsi = self.get_rsi(data_5min)
+                stochastic = self.get_stochastic(data_5min)
+
+                # Here add your conditions and logic to decide on the trade
+                decision = "buy"  # just an example, should be based on your conditions
+                self.enter_trade(decision)
+                self.submit_order()
+                self.check_position()
+                self.position_mode()
+                
+            elif general_trend == "sideways":
+                # Handle sideways or unclear trends, if necessary
+                pass
+
+            else:
+                # Handle other cases
+                pass
+        else:
+            print("Either position already exists or asset is not tradable.")
 
     def check_position(self):
         try:
@@ -26,8 +92,6 @@ class TradingStrategy:
     def load_data(self, timeframe):
         # Adjust the 'limit' parameter as per your requirements
         return api.get_barset(self.asset, timeframe, limit=100).df[self.asset]
-
-    # ... rest of your methods ...
 
     def enter_trade(self, decision):
         if decision == "buy":
@@ -53,5 +117,9 @@ class TradingStrategy:
         # This method can be used for more complex order logic, if needed
         pass
 
-
+if __name__ == "__main__":
+    asset_name = "YourAssetName"
+    strategy = TradingStrategy(asset_name)
+    strategy.decide_trade()
+    
 
