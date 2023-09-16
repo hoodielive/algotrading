58c035821ca0b1c12a1dc0c8130b5b29dad7cddc
hoodielive <hoodielive@gmail.com>
2023-09-16 18:32:50 -0400

    corrections

--- alpaca-dev/tradingbot/bot.py
+++ alpaca-dev/tradingbot/bot.py
@@ -1,24 +1,26 @@
 # Define the asset and output it as a String.
+    # Input: Keyboard
+    # Output: String
 
 # OGBE-ALPHA
 # Loop until timemout 2hrs.
 # Perform the initial check.
-# Check the position: ask the broker/API if we have an open position with asset.
+# Check the position->ask the broker/API if we have an open position with asset.
     # Input: String
     # Output: Boolean (True if exists and False if it does NOT exist)
         # If fails, return to OGBE
 
-# Check if asset is tradable: ask the broker/API if asset is tradable.
+# Check if asset is tradable->ask the broker/API if asset is tradable.
     # Input: String
     # Output: Boolean
         # If fails, return to OGBE
 
 # General Trend.
-# Load 30 min candles: demand the API to chart with 30min candles.
+# Load 30 min candles->demand the API to chart with 30min candles.
     # Input: (Whatever API needs), time range*, candle size*
     # Output: 30 mins candles (OHLC for every candle)
 
-# Perform general trade analysis: detect interesting trend (up/down/no-trend)
+# Perform general trade analysis->detect interesting trend (up/down/no-trend)
     # Input: 30 min candles data (close data)
     # Output: Up, Down, No trend Strings
     # If no trend defined, go back to start.
@@ -30,7 +32,7 @@
         # Output: boolean (True=confirmed, False=not confirmed)
         # If fails, return to OYEKU
 
-# Step 2->Perform instant trend analysis: confirm the trend detected by GT analysis
+# Step 2->Perform instant trend analysis->confirm the trend detected by GT analysis
         # Input: 5 min candles date (Close data), output of the GT analysis (Up/Down (String))
         # Output: boolean (True=confirmed, False=not-confirmed)
         # If fails, return to OYEKU
